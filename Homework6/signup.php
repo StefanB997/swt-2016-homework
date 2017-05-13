b2ec2fa33fbf7e3a262a2b40dcaaebe8fb6fb120
Stefan <stefanblazevski226@gmail.com>
2017-05-13 15:27:20 +0200

    Update

--- Homework6/signup.php
+++ Homework6/signup.php
@@ -9,16 +9,6 @@ if(true) {
 		$sql = "INSERT INTO  users (first_name, last_name, email, password, age) 
 		VALUES (:first_name, :last_name, :email, :password, :age)";
 
-		// $query = $db->prepare($sql);
-		// print_r ($query);
-		// $query->bindValue(':first_name', $_POST['first_name']);
-		// $query->bindValue(':last_name', $_POST['last_name']);
-		// $query->bindValue(':email', $_POST['email']);
-		// $query->bindValue(':password', $_POST['password']);
-		// $query->bindValue(':age', $_POST['age']);
-		// print_r ($r);
-
-
 		$query = $db->prepare($sql);
 		$query->bindValue(':first_name', $_POST['first_name']);
 		$query->bindValue(':last_name', $_POST['last_name']);
